182eed22d6bef1b045e12dcca9f703dcea45cebd
André Lam <andrelam@e.email>
2019-12-16 21:19:14 +0100

    Added enable aps script

---
+++ enableaps.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+#Put here all of the AP IDs 
+DeviceID="${DeviceID} xxxxxxxxxxxxxxxxxxxxxxxxx"
+#DeviceID="${DeviceID} yyyyyyyyyyyyyyyyyyyyyyyyy"  
+
+#user name of admin account on controller 
+username="your_username_here_between_quotes"   
+
+#password of admin account on controller 
+password="your_password_here_between_quotes"   
+
+#controller IP address and port 
+baseurl=https://192.168.1.32:8443
+
+#leave as default 
+site=default 
+
+cookie=/tmp/cookie 
+curl_cmd="curl -v --silent --cookie ${cookie} --cookie-jar ${cookie} --insecure" 
+${curl_cmd} --data "{\"username\":\"$username\", \"password\":\"$password\"}" $baseurl/api/login  
+
+for device in ${DeviceID}; 
+  do ${curl_cmd} -X PUT -H "Content-Type: application/json" --data "json={\"disabled\":false}" $baseurl/api/s/$site/rest/device/${device}
+done  
+
+${curl_cmd} $baseurl/logout
+
